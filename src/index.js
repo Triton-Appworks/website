a789c163856b827fc0708a9e728d5af096d0b279
Chris Meyer <chris@tritonappworks.com>
2025-08-29 08:39:31 -1000

    New buttons + animation improvements + cleanup

--- src/index.js
+++ src/index.js
@@ -3,20 +3,29 @@ import './css/button.css';
 import './css/index.css';
 import logo from './images/TritonAppworks-blue.png';
 
-const contactClick = () => {
-  window.location.href = 'mailto:chris@tritonappworks.com'
+const appsClick = (event) => {
+  event.target.blur();
+};
+const utilitiesClick = (event) => {
+  event.target.blur();
+};
+const contactClick = (event) => {
+  window.location.href = "mailto:chris@tritonappworks.com";
+  event.target.blur();
 };
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className="homepage">
-    <div className="overlay fade-in"></div>
-    <div className="logo-container fade-in delay zoom-in">
+  <div className="homepage fade-in">
+    <div className="overlay fade-in-delay1s"></div>
+    <div className="logo-container fade-in-delay2s zoom-in-delay1s">
       <img src={logo} alt="Triton Appworks LLC" className="logo" />
     </div>
-    <div className='button-container'>
+    <div className="button-container fade-in-delay3s zoom-in-delay2s">
+      <button onClick={appsClick} className="button applications apps-button">Apps</button>
+      <button onClick={utilitiesClick} className="button tools utils-button">Utilities</button>
       <button onClick={contactClick} className="button email contact-button">Contact</button>
     </div>
-    <footer className="footer">
+    <footer className="footer fade-in-delay4s zoom-in-delay3s">
       &copy; {new Date().getFullYear()} Triton Appworks LLC
     </footer>
   </div>
